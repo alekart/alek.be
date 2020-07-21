317a35ce91faae63f5a7478b2ead8f94f4a04a60
alek <alekart@users.noreply.github.com>
2020-07-21 13:00:46 +0200

    Publish next versions via release script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,5 +1,7 @@
 const {execSync} = require('child_process');
 const git = require('git-rev-sync');
+const argv = process.argv.slice(2);
+let next = argv.length > 0 && argv[0] === 'next';
 
 /**
  * Preconfigured execSync
@@ -14,11 +16,18 @@ const version = git.isTagDirty()
   : `@ v${git.tag()}`;
 
 exec('yarn build');
-exec('git checkout gh-pages');
-exec('git reset origin/gh-pages --hard');
-exec('git rm -r -f --ignore-unmatch ./*');
-exec('mv ./dist/* ./');
-exec('git add -A');
-exec(`git commit -m "Publish website ${version}"`);
+exec('git checkout gh-pages --force');
+exec('git fetch && git reset origin/gh-pages --hard');
+if(next){
+  exec('git rm -r -f --ignore-unmatch ./next/*');
+  exec('mkdir -p next');
+  exec('mv ./dist/* ./next/');
+
+} else {
+  exec('git rm -r -f --ignore-unmatch "!./next/* ./*"');
+  exec('mv ./dist/* ./');
+}
+exec('git add --all');
+exec(`git commit -m "Publish${next ? ' next' : ''} version ${version}"`);
 exec('git push');
 exec('git checkout master');
