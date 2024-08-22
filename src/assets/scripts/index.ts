13bd3d832f88efd6eef33e8d56cc67b9091fd4b3
alek <alekart@users.noreply.github.com>
2024-08-22 16:40:48 +0200

    Update track script

--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -15,17 +15,16 @@ declare global {
 (() => {
   function track(element: HTMLElement) {
     const data = element.getAttribute('data-t');
-    (window as any).dataLayer?.push({'event': data})
+    window.gtag('event', 'click', {
+      event_category: 'Click',
+      event_label: data || 'element clicked',
+    });
   }
 
   document.body.onclick = (event: MouseEvent) => {
     const trackedTarget: HTMLElement | null = (event.target as HTMLElement).closest('[data-t]');
     if(trackedTarget) {
-      console.log('click', trackedTarget.getAttribute('data-t'));
-      window.gtag('event', 'click', {
-        event_category: 'click',
-        event_label: trackedTarget.getAttribute('data-t') || 'element clicked',
-      });
+      track(trackedTarget);
     }
   }
 })();
