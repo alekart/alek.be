4fc0c6d8b5c098e503fd397243f6f07b459c1745
alek <2306207+alekart@users.noreply.github.com>
2024-09-01 12:56:11 +0200

    Track link clicks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
-import { TrackClickDirective } from './directives/track-click.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TrackClickDirective, RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/links/links.component.ts
+++ src/app/links/links.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TrackClickDirective } from '../directives/track-click.directive';
 
 @Component({
   selector: 'app-links',
   standalone: true,
-  imports: [],
+  imports: [TrackClickDirective],
   templateUrl: './links.component.html',
   styleUrl: './links.component.scss'
 })
