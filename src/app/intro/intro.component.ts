b3fdeaa2f2a6f534fa9a42888672b0727048e6f7
alek <2306207+alekart@users.noreply.github.com>
2024-09-01 12:56:11 +0200

    Track link clicks

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LinksComponent } from '../links/links.component';
+import { TrackClickDirective } from '../directives/track-click.directive';
 
 @Component({
   selector: 'app-intro',
   standalone: true,
-  imports: [LinksComponent],
+  imports: [LinksComponent, TrackClickDirective],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss'
 })
